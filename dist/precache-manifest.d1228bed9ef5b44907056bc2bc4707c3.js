41a43a79128b318bed466bb7a358b66fac490123
Nick Gronow <nick@wov.io>
2019-05-13 22:54:39 -0400

    Add dist directory

---
+++ dist/precache-manifest.d1228bed9ef5b44907056bc2bc4707c3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "92e938d9cb850c0c7f6cee940266ab6c",
+    "url": "/feather-sprite.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83b6170c2cb78c2b1d729d30f55e0907",
+    "url": "/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "beedcd6d14dc37ef4158",
+    "url": "/assets/css/chunk-vendors.b781389f.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/assets/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "1ef4ce8d94c4c68adcf4a99594f8eca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9915fd87ad2f2ce7d0a49ac2d0bc65c1",
+    "url": "/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "215e0161486c1f04acab54d3746aae49",
+    "url": "/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "fc78e5ff8ca6bb46be6a",
+    "url": "/assets/js/app.309a831d.js"
+  },
+  {
+    "revision": "beedcd6d14dc37ef4158",
+    "url": "/assets/js/chunk-vendors.6c6ec66c.js"
+  },
+  {
+    "revision": "aa9f042312db8c8bdfdd7c2b4b458fab",
+    "url": "/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "005ba6938b06bc1738a4ccbf2488c0a9",
+    "url": "/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "8aee9164a41b526d310421566fdd14b8",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "efe33c7bb4e59db14ab95b9fc0d41cfe",
+    "url": "/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "a58246bd64bbf6064a45253b7a17b1cb",
+    "url": "/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "fc78e5ff8ca6bb46be6a",
+    "url": "/assets/css/app.a24d5f85.css"
+  }
+];
